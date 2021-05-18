5e6e90d2b07d40d1df4bb4bd3832648e1c4371f1
Wallacy Silva <wallacyc11@gmail.com>
2021-05-18 12:02:46 -0300

    Ajuste de tags

--- casacodigo/src/main/java/br/com/zup/casacodigo/controller/AutorController.java
+++ casacodigo/src/main/java/br/com/zup/casacodigo/controller/AutorController.java
@@ -1,17 +1,30 @@
 package br.com.zup.casacodigo.controller;
 
+<<<<<<< HEAD
+=======
+import java.net.URI;
+
+>>>>>>> ceaff7cbb20a629e35fef03fb596991c7821ccc6
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+<<<<<<< HEAD
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+=======
+>>>>>>> ceaff7cbb20a629e35fef03fb596991c7821ccc6
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+<<<<<<< HEAD
 
 import br.com.zup.casacodigo.config.validacao.ProibeEmeailDuplicadoValidator;
+=======
+import org.springframework.web.util.UriComponentsBuilder;
+
+>>>>>>> ceaff7cbb20a629e35fef03fb596991c7821ccc6
 import br.com.zup.casacodigo.controller.dto.AutorDto;
 import br.com.zup.casacodigo.controller.form.AutorForm;
 import br.com.zup.casacodigo.modelo.Autor;
@@ -24,6 +37,7 @@ public class AutorController {
 	@Autowired
 	private AutorRepository autorRepository;
 	
+<<<<<<< HEAD
 	@Autowired
 	private ProibeEmeailDuplicadoValidator proibeEmeailDuplicadoValidator;
 	
@@ -37,5 +51,13 @@ public class AutorController {
 		Autor autor = autorForm.conveter(autorForm);
 		autorRepository.save(autor);
 		return ResponseEntity.ok().build();
+=======
+	@PostMapping
+	public ResponseEntity<AutorDto> cadastrar(@RequestBody @Valid AutorForm autorForm, UriComponentsBuilder uriBuilder){
+		Autor autor = autorForm.conveter(autorForm);
+		autorRepository.save(autor);
+		URI uri = uriBuilder.path("/autor/{id}").buildAndExpand(autor.getId()).toUri();
+		return ResponseEntity.created(uri).body(new AutorDto(autor));
+>>>>>>> ceaff7cbb20a629e35fef03fb596991c7821ccc6
 	}
 }
--- casacodigo/src/main/java/br/com/zup/casacodigo/repository/AutorRepository.java
+++ casacodigo/src/main/java/br/com/zup/casacodigo/repository/AutorRepository.java
@@ -1,7 +1,10 @@
 package br.com.zup.casacodigo.repository;
 
+<<<<<<< HEAD
 import java.util.Optional;
 
+=======
+>>>>>>> ceaff7cbb20a629e35fef03fb596991c7821ccc6
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +12,10 @@ import br.com.zup.casacodigo.modelo.Autor;
 
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long>{
+<<<<<<< HEAD
 
 	Optional<Autor> findByEmail(String email);
+=======
+>>>>>>> ceaff7cbb20a629e35fef03fb596991c7821ccc6
 	
 }
